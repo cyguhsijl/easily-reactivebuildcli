200c62ed0eb4e29d4293d7756e851fc2c818b9cb
cyguhsijl <felixcajipsg@hotmail.com>
2024-05-11 08:46:23 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) return [];
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-  const result = [];
-  backtrack("", 0);
-  return result;
-  function backtrack(current, index) {
-    if (current.length === digits.length) {
-      result.push(current);
-      return;
+function isValidBST(root) {
+  const stack = [];
+  let inorder = -Infinity;
+  while (stack.length || root) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
     }
-    const letters = map[digits[index]];
-    for (const letter of letters) {
-      backtrack(current + letter, index + 1);
-    }
+    root = stack.pop();
+    if (root.val <= inorder) return false;
+    inorder = root.val;
+    root = root.right;
   }
+  return true;
 }
